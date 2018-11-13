346466b474ffd19cb1c324c1109500c5702f3287
Alexander Thompson <legoboyjr@gmail.com>
2018-11-13 18:52:00 -0500

    added tackyon to page and made sure it worked

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>fun wow</h1>
+        <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">Button Text</a>
       </div>
     );
   }
