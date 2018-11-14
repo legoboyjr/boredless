a13c72bdc9d83d0952d32b05a29ff42168c4d322
Alexander Thompson <legoboyjr@gmail.com>
2018-11-13 19:19:04 -0500

    fancied up the page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>fun wow</h1>
-        <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">Button Text</a>
+      <div className="vh-100 bg-black flex justify-center items-center">
+        <button className="f1 outline-transparent dim ph3 pv2 mb2 dib white bg-black b--none br-100 h5 w5 bg-blue">Bored?</button>
       </div>
     );
   }
